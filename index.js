8e833282e7532b0950811faaa5cdc342a088ff0e
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-04-24 21:53:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var katzDeliLine = [];
-
 function takeANumber(deliLine, name){
   deliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line`;
   }
 
-function nowServing
+function nowServing(){
+  
+}
